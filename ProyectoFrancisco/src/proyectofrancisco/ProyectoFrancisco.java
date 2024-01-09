a5c3ff9da393f4184d489161d836f3d27d56b4f7
jocarsa <info@josevicentecarratala.com>
2024-01-09 19:41:58 +0100

    Añado una primera linea

--- ProyectoFrancisco/src/proyectofrancisco/ProyectoFrancisco.java
+++ ProyectoFrancisco/src/proyectofrancisco/ProyectoFrancisco.java
@@ -15,6 +15,7 @@ public class ProyectoFrancisco {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola soy la linea 1");
     }
     
 }
